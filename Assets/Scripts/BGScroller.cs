bc25878d20eee60fca6f3db5c33923a6d1c1a80c
Piwi <piwi@MBPdeUtlisateur.lan>
2022-10-12 23:44:40 +0200

    Added scrolling bg

---
+++ Assets/Scripts/BGScroller.cs
@@ -0,0 +1,34 @@
+﻿// scrolls a quad object
+using UnityEngine;
+using System.Collections;
+
+public class BGScroller : MonoBehaviour
+{
+    public float scrollSpeed;
+    public GameObject prefab;
+
+    private float length;
+    private Vector3 startPos;
+    private Quaternion startRot;
+    private GameObject clone;
+
+    void Awake()
+    {
+        length = GetComponent<BoxCollider2D>().bounds.size.x;
+        startPos = transform.position;
+        startRot = transform.rotation;
+        clone = Instantiate(prefab, new Vector3(startPos.x + length, startPos.y, startPos.z), startRot);
+        clone.transform.localScale = transform.localScale;
+    }
+
+    void Update()
+    {
+        prefab.transform.position += Vector3.left * Time.deltaTime * scrollSpeed;
+        clone.transform.position += Vector3.left * Time.deltaTime * scrollSpeed;
+        if (clone.transform.position.x <= startPos.x)
+        {
+            prefab.transform.position = startPos;
+            clone.transform.position = new Vector3(startPos.x + length, startPos.y, startPos.z);
+        }
+    }
+}
